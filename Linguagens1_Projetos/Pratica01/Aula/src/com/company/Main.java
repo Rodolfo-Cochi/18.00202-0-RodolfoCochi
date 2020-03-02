daa88f701f6e1be8832144164958035c84fec348
Rodolfo-Cochi <rodolfocochi@gmail.com>
2020-03-02 12:56:24 -0300

    Aula 02 - Java

---
+++ Linguagens1_Projetos/Pratica01/Aula/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        double n1, n2;
+        System.out.println("Informe dois valores: ");
+        Scanner scanner = new Scanner (System.in);
+        n1 = scanner.nextDouble();
+        n2 = scanner.nextDouble();
+        if(n1>n2){
+            System.out.println("n1 maior");
+        }else{
+            System.out.println("n2 maior");
+        }
+        }
+    }
+
