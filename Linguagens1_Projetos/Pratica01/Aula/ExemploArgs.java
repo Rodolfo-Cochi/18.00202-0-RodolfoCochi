6d970ea9c7743310924cd623584456de5783b38b
Rodolfo-Cochi <rodolfocochi@gmail.com>
2020-03-02 11:43:23 -0300

    corecao2

--- Linguagens1_Projetos/Pratica01/Aula/ExemploArgs.java
+++ Linguagens1_Projetos/Pratica01/Aula/ExemploArgs.java
@@ -2,7 +2,7 @@ class ExemploArgs {
 
     public static void main (String[] args) {
         int i;
-        for(i = 0; i < args.lenght; i++) {
+        for(i = 0; i < args.length; i++) {
             System.out.println(args[i]);
         }
     }
