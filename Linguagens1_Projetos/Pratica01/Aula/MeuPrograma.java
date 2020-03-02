23259dd2fc6d4a9c35024542c6642773ed3c1b42
Rodolfo-Cochi <rodolfocochi@gmail.com>
2020-03-02 11:28:17 -0300

    Arquivo Compilado

---
+++ Linguagens1_Projetos/Pratica01/Aula/MeuPrograma.java
@@ -0,0 +1,8 @@
+class MeuPrograma {
+	public static void main(String[] args) {
+		//Mensagem de saindo do sistema
+		System.out.println("Minha primeira Aplicacao Java!!");
+		System.out.println("Ola Mundo");
+		System.out.println("Modo Connan");
+	}
+}
