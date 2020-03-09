20d81f68329ee65af28942db1e3ae9c7497ea263
Rodolfo-Cochi <rodolfocochi@gmail.com>
2020-03-09 12:36:04 -0300

    Aula de POO

---
+++ Linguagens1_Projetos/Pratica2/Exemplo1/Conta.java
@@ -0,0 +1,39 @@
+
+public class Conta{
+
+    //Atributos da Classe
+    public int numero;
+    public Cliente cliente;
+    public double saldo;
+
+    //Metodos da classe
+
+    boolean sacar(double valor) {
+        if (this.saldo < valor) {
+            return false;
+        }
+        else {
+            this.saldo = this.saldo - valor;
+            return true;
+        }
+    }
+
+    void depositar(double valor){
+        this.saldo += valor;
+    }
+
+    void vizualizarSaldo() {
+        System.out.println("Saldo:R$ " + this.saldo);
+    }
+
+    boolean transferirDinheiro(Conta Destino, double valor){
+        if(sacar(valor)){
+            Destino.depositar(valor); 
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}
---
+++ Linguagens1_Projetos/Pratica2/Exemplo1/ContaTestDrive.java
@@ -0,0 +1,35 @@
+public class ContaTestDrive{
+    public static void main(String[] args) {
+        //Declarar variaveis
+        Conta c1;
+        Conta c2;
+
+        //Instancia Conta - coloca a referencia de um objeto
+        c1 = new Conta();
+        c2 = new Conta();
+
+        //Info c1
+        c1.cliente = new Cliente();
+        c1.cliente.nome = "Kirishima";
+        c1.saldo = 2000;
+
+        //Info c2
+        c2.cliente = new Cliente();
+        c2.cliente.nome = "Yoh";
+        c2.saldo = 500;
+
+        //Operacoes c1
+        System.out.println("" + c1.cliente.nome);
+        c1.vizualizarSaldo();
+        c1.sacar(500);
+        c1.transferirDinheiro(c2, 500);
+        c1.vizualizarSaldo();
+
+        //Operacoes c2
+        System.out.println("" + c2.cliente.nome);
+        c2.vizualizarSaldo();
+
+        
+
+    }
+}
