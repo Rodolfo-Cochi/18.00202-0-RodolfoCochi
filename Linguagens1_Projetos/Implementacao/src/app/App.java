faa35f03c3fe42697ae792a8333d9a95b780223e
Rodolfo-Cochi <rodolfocochi@gmail.com>
2020-05-14 12:24:56 -0300

    Mudancas classpath

--- Linguagens1_Projetos/Implementacao/src/app/App.java
+++
@@ -1,8 +0,0 @@
-package app;
-import pacote.Dog;
-public class App {
-    public static void main(String[] args) throws Exception {
-        Dog dog1 = new Dog();
-        dog1.latir();
-    }
-}
--- Linguagens1_Projetos/Implementacao/src/pacote/Dog.java
+++
@@ -1,7 +0,0 @@
-package pacote;
-
-public class Dog {
-    public void latir(){
-        System.out.println("Au au");
-    }
-}
