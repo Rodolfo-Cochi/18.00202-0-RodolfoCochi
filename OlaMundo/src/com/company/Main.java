b4771cf512554926e00893fd6c2074adf1d41924
Rodolfo-Cochi <61150908+Rodolfo-Cochi@users.noreply.github.com>
2020-06-09 03:28:13 -0300

    Testando o IntelliJ com Git parte 2

--- OlaMundo/src/com/company/Main.java
+++ OlaMundo/src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Ola Mundo");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu nome");
+        String nome = scanner.nextLine();
     }
 }
