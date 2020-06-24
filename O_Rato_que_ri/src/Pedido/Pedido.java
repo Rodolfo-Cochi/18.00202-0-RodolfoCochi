b5acdf0523468bc0c974a7ff61b6de4a4b9ba0b8
Rodolfo-Cochi <61150908+Rodolfo-Cochi@users.noreply.github.com>
2020-06-24 13:05:00 -0300

    Gerador de id

--- O_Rato_que_ri/src/Pedido/Pedido.java
+++ O_Rato_que_ri/src/Pedido/Pedido.java
@@ -1,5 +1,16 @@
 package Pedido;
 
+import java.util.Random;
+
 public class Pedido {
     String id;
+
+    private String geradorId(){
+        Random random = new Random();
+        String idGerado = "";
+        for(int i = 0; i < 3; i++)
+            idGerado += random.nextInt(10);
+        return idGerado;
+    }
 }
+
