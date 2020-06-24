869ee0cfeb6c86cce23b2bf0ab5960cc304aba7c
Rodolfo-Cochi <61150908+Rodolfo-Cochi@users.noreply.github.com>
2020-06-24 13:46:59 -0300

    Criacao do metodo menu

--- O_Rato_que_ri/src/com/company/Main.java
+++ O_Rato_que_ri/src/com/company/Main.java
@@ -1,23 +1,16 @@
 package com.company;
 
+import java.awt.*;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         int op;
-
-
+        Scanner scanner = new Scanner(System.in);
         do {
-            System.out.println("Bem vindo a Rato que Ri");
-            System.out.println("1 - Nova Venda");
-            System.out.println("2 - Verificar pedidos");
-            System.out.println("3 - Alterar pedidos");
-            System.out.println("0 - Sair");
-            System.out.println("Digite sua opcao: ");
+            Menu();
             op = scanner.nextInt();
-
             switch (op) {
                 case 1:
                     System.out.println("Deu bom!");
@@ -35,5 +28,14 @@ public class Main {
                     break;
             }
         }while(op != 0);
+    }
+
+    public static void Menu(){
+        System.out.println("Bem vindo a Rato que Ri");
+        System.out.println("1 - Nova Venda");
+        System.out.println("2 - Verificar pedidos");
+        System.out.println("3 - Alterar pedidos");
+        System.out.println("0 - Sair");
+        System.out.println("Digite sua opcao: ");
     }
 }
