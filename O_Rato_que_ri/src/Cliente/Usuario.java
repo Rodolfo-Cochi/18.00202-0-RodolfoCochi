4a1d4ed2d0dd3b7262fb0dab7280e277e5d6bcb7
Rodolfo-Cochi <61150908+Rodolfo-Cochi@users.noreply.github.com>
2020-06-24 12:57:18 -0300

    Criacao da classe de usuario

---
+++ O_Rato_que_ri/src/Cliente/Usuario.java
@@ -0,0 +1,12 @@
+package Cliente;
+
+public class Usuario {
+    String senha, email, nome;
+
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+}
