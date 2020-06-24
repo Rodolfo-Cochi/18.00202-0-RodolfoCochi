d119547cf168af0a683a6fef5c8d4ab86d939aa1
Rodolfo-Cochi <61150908+Rodolfo-Cochi@users.noreply.github.com>
2020-06-24 17:40:49 -0300

    ajuste da main e criacao do case 2 (vizualizacao)

--- O_Rato_que_ri/src/Pedido/Pedido.java
+++ O_Rato_que_ri/src/Pedido/Pedido.java
@@ -6,8 +6,8 @@ import Pedido.Enum.FormaPagamento;
 import java.util.Random;
 
 public class Pedido {
-    Enum State;
-    String Id;
+    Enum State, fpag;
+    String Id, descricao, valor;
 
     public Enum getState() {
         return State;
@@ -17,11 +17,41 @@ public class Pedido {
         return Id;
     }
 
-    public Pedido(String descricao, String formadepagamento, String valor) {
-        State = Estado.REALIZADO;
-        Id = geradorId();
+    public Enum getFpag() {
+        return fpag;
     }
 
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public Pedido(String descricao, int formadepagamento, String valor) {
+        switch (formadepagamento) {
+            case 1:
+                fpag = FormaPagamento.CREDITO;
+                break;
+            case 2:
+                fpag = FormaPagamento.DEBITO;
+                break;
+            case 3:
+                fpag = FormaPagamento.DINHEIRO;
+                break;
+            case 4:
+                fpag = FormaPagamento.VALE_ALIMENTACAO;
+                break;
+            case 5:
+                fpag = FormaPagamento.VALE_REFEICAO;
+                break;
+        }
+        this.State = Estado.REALIZADO;
+        this.Id = geradorId();
+        this.descricao = descricao;
+        this.valor = valor;
+    }
 
 
     private String geradorId() {
--- O_Rato_que_ri/src/com/company/Main.java
+++ O_Rato_que_ri/src/com/company/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Pedido> pedidos = new ArrayList<Pedido>();
+        ArrayList<Pedido> pedidos = new ArrayList();
         int op;
         Scanner scanner = new Scanner(System.in);
         String desc, valor, nome, email, senha;
@@ -30,11 +30,8 @@ public class Main {
         Usuario user = new Usuario(nome, email, senha);
 
         do {
-            //saber quem esta logado
-            System.out.println("\nUsuario: " + nome);
-
-            //print do menu
-            Menu();
+            System.out.println("\nUsuario: " + nome);  //saber quem esta logado
+            Menu();  //print do menu
             op = Integer.parseInt(scanner.nextLine());
             switch (op) {
                 case 1:
@@ -46,70 +43,34 @@ public class Main {
                         valor = scanner.nextLine();
 
                         System.out.println("Qual a forma de pagamento? \n 1 - Credito \n 2 - Debito \n 3 - Vale alimentacao \n 4 - Vale refeicao");
-
                         pag = Integer.parseInt(scanner.nextLine());
-                        String fpag;
 
-                        switch (pag) {
-                            case 1:
-                                fpag = "Credito";
-                                Pedido p1 = new Pedido(desc, fpag, valor);
-                                pedidos.add(p1);
-
-                                System.out.println("Id: " + p1.getId());
-                                System.out.println("Estado do pedido: " + p1.getState());
-                                System.out.println("Descricao do pedido: " + desc);
-                                System.out.println("Forma de Pagamento: " + fpag);
-                                System.out.println("Valor: R$ " + valor);
-
-                                break;
-                            case 2:
-                                fpag = "Debito";
-                                Pedido p2 = new Pedido(desc, fpag, valor);
-                                pedidos.add(p2);
-
-                                System.out.println("Id: " + p2.getId());
-                                System.out.println("Estado do pedido: " + p2.getState());
-                                System.out.println("Descricao do pedido: " + desc);
-                                System.out.println("Forma de Pagamento: " + fpag);
-                                System.out.println("Valor: R$ " + valor);
-                                break;
-                            case 3:
-                                fpag = "Vale alimentacao";
-                                Pedido p3 = new Pedido(desc, fpag, valor);
-                                pedidos.add(p3);
-
-                                System.out.println("Id: " + p3.getId());
-                                System.out.println("Estado do pedido: " + p3.getState());
-                                System.out.println("Descricao do pedido: " + desc);
-                                System.out.println("Forma de Pagamento: " + fpag);
-                                System.out.println("Valor: R$ " + valor);
-                                break;
-                            case 4:
-                                fpag = "Vale refeicao";
-                                Pedido p4 = new Pedido(desc, fpag, valor);
-                                pedidos.add(p4);
-
-                                System.out.println("Id: " + p4.getId());
-                                System.out.println("Estado do pedido: " + p4.getState());
-                                System.out.println("Descricao do pedido: " + desc);
-                                System.out.println("Forma de Pagamento: " + fpag);
-                                System.out.println("Valor: R$ " + valor);
-                                break;
-                        }
-                    }else{
-                        System.out.println("Infelizmente voce nao tem acesso");
+                        Pedido p = new Pedido(desc, pag, valor);
+                        pedidos.add(p);
+                        System.out.println("\nPedido numero: " + p.getId());
+                        System.out.println("Estado do pedido: " + p.getState());
+                        System.out.println("Descricao do pedido: " + desc);
+                        System.out.println("Forma de Pagamento: " + p.getFpag());
+                        System.out.println("Valor: R$ " + valor);
+                    } else {
+                        System.out.println("Infelizmente voce nao tem acesso.");
                     }
-
 
                     break;
 
                 case 2:
-                    System.out.println("Deu bom 2");
+                    for (Pedido ped: pedidos) {
+                        System.out.println("Id:"+ ped.getId() + "\tPreco:R$" + ped.getValor() + "\tForma de pagamento:" + ped.getFpag() + "\tEstado atual:" + ped.getState() + "\tDescricao:" + ped.getDescricao());
+                    }
                     break;
 
                 case 3:
-                    System.out.println("Deu bom 3");
+                    if (user.comparador(senha)) {
+                        System.out.println("O que deseja alterar no Pedido: ");
+                        System.out.println("1 - Mudar Estado\n 2 - Mudar Descricao\n 3 - Mudar Forma de pagamento \n 4 - Mudar preco");
+                    } else {
+                        System.out.println("Infelizmente voce nao tem acesso.");
+                    }
                     break;
                 case 0:
                     System.out.println("Tchau!");
@@ -117,6 +78,13 @@ public class Main {
             }
         } while (op != 0);
     }
+
+
+
+
+
+
+
 
     public static void Menu() {
         System.out.println("\nLogado em Rato que Ri");
