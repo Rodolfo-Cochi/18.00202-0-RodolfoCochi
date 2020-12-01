9dfb019a6ebcc5a2144b38eb2284af7c80fe7c23
Rodolfo-Cochi <61150908+Rodolfo-Cochi@users.noreply.github.com>
2020-12-01 00:21:27 -0300

    Arrumando os botoes da pagina 2

--- Flutter/flutter_app/lib/Screen/screen2.dart
+++ Flutter/flutter_app/lib/Screen/screen2.dart
@@ -5,29 +5,103 @@ class Screen2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
+        resizeToAvoidBottomPadding: false,
         appBar: AppBar(
           centerTitle: true,
-          title: const Text("Poke Guesser",
-            style: TextStyle(fontFamily: "Pokemon"),),
+          title: const Text(
+            "Poke Guesser",
+            style: TextStyle(fontFamily: "Pokemon"),
+          ),
         ),
         body: SingleChildScrollView(
             child: Stack(
-              children: [
-                Image.network("https://wallpapercave.com/wp/wp2609505.png",
-                  fit: BoxFit.cover,
-                  alignment: Alignment.center,
+          children: [
+            Image.network(
+              "https://www.teahub.io/photos/full/68-682392_28-04-2018-pokemon-sun-and-moon-wallpaper.jpg",
+              fit: BoxFit.cover,
+              alignment: Alignment.center,
+            ),
+            Image.network(
+              "https://static.quizur.com/i/b/57c1c26fc0b812.5998420157c1c26fb156c9.51498011.png",
+              fit: BoxFit.cover,
+              alignment: Alignment.center,
+            ),
+            new Positioned(
+              left: 40.0,
+              bottom: 330.0,
+              height: 60,
+              width: 330,
+              child: ElevatedButton(
+                onPressed: null,
+                child: Text(
+                  "Pokémon 1",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35.0,
+                    color: Colors.black,
+                  ),
                 ),
-                new Positioned(
-                    left: 100.0,
-                    bottom: 150.0,
-                    width: 200.0,
-                    child: new ElevatedButton(onPressed: null, child: Text("Play !",
-                      style: new TextStyle(
-                          fontFamily: "Pokemon",
-                          fontSize: 50.0
-                      ),)))
+              ),
+            ),
 
-              ],
-            )));
+            // SEGUNDO BOTAO
+            Positioned(
+              left: 40.0,
+              bottom: 250.0,
+              height: 60,
+              width: 330,
+              child: ElevatedButton(
+                onPressed: null,
+                child: Text(
+                  "Pokémon 2",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+
+            // TERCEIRO BOTAO
+            Positioned(
+              left: 40.0,
+              bottom: 170.0,
+              height: 60,
+              width: 330,
+              child: ElevatedButton(
+                onPressed: null,
+                child: Text(
+                  "Pokémon 3",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+
+            // QUARTO BOTAO
+            Positioned(
+              left: 40.0,
+              bottom: 90.0,
+              height: 60,
+              width: 330,
+              child: ElevatedButton(
+                onPressed: null,
+                child: Text(
+                  "Pokémon 4",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )));
   }
 }
