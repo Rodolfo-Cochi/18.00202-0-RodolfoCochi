eae6233a3ba1e8b80c7b0b76bac8fd5607e0023d
Rodolfo-Cochi <61150908+Rodolfo-Cochi@users.noreply.github.com>
2020-12-01 14:37:13 -0300

    Criando conexao com api

--- Flutter/flutter_app/lib/Screen/screen1.dart
+++ Flutter/flutter_app/lib/Screen/screen1.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_app/Screen/screen2.dart';
 
 class Screen1 extends StatelessWidget {
@@ -8,30 +9,50 @@ class Screen1 extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: const Text("Poke Guesser",
-          style: TextStyle(fontFamily: "Pokemon"),),
+          title: const Text(
+            "Poke Guesser",
+            style: TextStyle(fontFamily: "Pokemon"),
+          ),
         ),
         body: SingleChildScrollView(
             child: Stack(
           children: [
-            Image.network("https://wallpapercave.com/wp/wp2609505.png",
-            fit: BoxFit.cover,
-            alignment: Alignment.center,
+            Image.network(
+              "https://wallpapercave.com/wp/wp2609505.png",
+              fit: BoxFit.cover,
+              alignment: Alignment.center,
             ),
             new Positioned(
-                left: 100.0,
-                bottom: 150.0,
-                width: 200.0,
-                child: new ElevatedButton(onPressed: (){
-                  Navigator.push(context,
-                  MaterialPageRoute(builder:(context) => Screen2()),
+              left: 100.0,
+              bottom: 150.0,
+              width: 200.0,
+              child: new ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Screen2()),
                   );
-                }, child: Text("Play !",
-                style: new TextStyle(
-                  fontFamily: "Pokemon",
-                  fontSize: 50.0
-                ),)))
+                },
+                child: Text(
+                  "Play !",
+                  style: new TextStyle(fontFamily: "Pokemon", fontSize: 50.0),
+                ),
+              ),
+            ),
+            Positioned(
+                left: 100.0,
+                bottom: 50.0,
+                width: 200.0,
+                child: new ElevatedButton(
+                    onPressed: (){
+                      SystemNavigator.pop();
+                    },
 
+                    child: Text(
+                      "Exit !",
+                      style:
+                          new TextStyle(fontFamily: "Pokemon", fontSize: 50.0),
+                    )))
           ],
         )));
   }
